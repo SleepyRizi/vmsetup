d4562f370e5f1997bb9fa08cdefbedfb562238ab
Muhammad <rizwanali.sleepy@gmail.com>
2024-05-02 09:37:42 +0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "project" {
+  default = null
+}
+
+variable "credentials_file" {
+  default = null
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "coin_name" {
+  default = "ETC"
+}
+
+variable "wallet_address" {
+  default = "0xRCN9moqFh8GZXcPFqKSioVqdGwYA76kMSe"
+}
+
+variable "gpu_types" {
+  default = ["t4", "a100", "v100"]
+}
+
+variable "group_size" {
+  default = 16
+}
+
+variable "provisioning_models" {
+  default = ["SPOT", "STANDARD"]
+}
+
+variable "prefix" {
+  default = ""
+}
